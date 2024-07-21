3e5e7090ce4a316c4b662c73e781c6d274b6d495
hellodamii <daremiarchs@gmail.com>
2024-07-21 03:08:08 +0100

    Initial Commit

---
+++ iOS18 Photos App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS18 Photos App
+//
+//  Created by Busha on 21/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS18 Photos App/iOS18_Photos_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS18_Photos_AppApp.swift
+//  iOS18 Photos App
+//
+//  Created by Busha on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS18_Photos_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
